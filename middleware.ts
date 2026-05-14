34614cf567f1b26629ed28bc0f5a997111308eaf
Bruno Morilla <morilla.bsm@gmail.com>
2026-05-13 23:16:17 -0300

    fix middleware types

--- middleware.ts
+++ middleware.ts
@@ -16,7 +16,13 @@ export async function middleware(request: NextRequest) {
           return request.cookies.getAll();
         },
 
-        setAll(cookiesToSet) {
+        setAll(
+          cookiesToSet: {
+            name: string;
+            value: string;
+            options?: Record<string, unknown>;
+          }[]
+        ) {
           cookiesToSet.forEach(({ name, value }) =>
             request.cookies.set(name, value)
           );
